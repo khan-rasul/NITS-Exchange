edda6f53f85f5aebb02dc2d30bcbfb17a5cc3444
Neetya <neetyapasari09@gmail.com>
2020-09-07 22:43:48 +0530

    added comment popup forms

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,25 @@
-$(".cancel").click(function() {
-  $(this).fadeOut();
+function openNewForm() {
+  $("#commentForm").css("display", "block");
+}
+
+function closeNewForm() {
+  $("#commentForm").css("display", "none");
+}
+
+function openEditForm() {
+  $("#editForm").css("display", "block");
+}
+
+function closeEditForm() {
+  $("#editForm").css("display", "none");
+}
+
+$('#inputboxNew').on('input', function () {
+  $(this).height(0).height(this.scrollHeight);
+  $('#commentForm').height(this.scrollHeight + 115);
 });
 
-$(".submit").click(function() {
-    $(this).parent().find("input").removeAttr("disabled");
-    $(this).parent().find(".toggle").fadeIn();
+$('#inputboxEdit').on('input', function () {
+  $(this).height(0).height(this.scrollHeight);
+  $('#editForm').height(this.scrollHeight + 115);
 });
